a18448dcff5dafe17875f59513d50ac421d23bd4
Clément Touzet <ctouzet001@iutbayonne.univ-pau.fr>
2022-01-08 17:47:07 +0100

    Génération de données aléatoire pour toutes les tables de la base de données

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,7 +4,11 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+
 use App\Entity\Entreprise;
+use App\Entity\Stage;
+use App\Entity\Formation;
+
 
 class AppFixtures extends Fixture
 {
@@ -13,13 +17,69 @@ class AppFixtures extends Fixture
         //Création d'un générateur de données Faker
         $faker = \Faker\Factory::create('fr_FR');
 
-        $entrepriseAleatoire = new Entreprise();
-        $entrepriseAleatoire->setNom($faker->company);
-        $entrepriseAleatoire->setAdresse($faker->address);
-        $entrepriseAleatoire->setActivite("activite");
-        $entrepriseAleatoire->setUrlSite("test");
-        $manager->persist($entrepriseAleatoire);
+        //creation des données de Entreprise
+        $tabEntreprise = array();
+        for($i = 0; $i < 10; $i++){
 
+            $nomEntreprise = $faker->company;
+            $idEntreprise = $i+1;
+
+            $entrepriseAleatoire = new Entreprise();
+            $entrepriseAleatoire->setNom($nomEntreprise);
+            $entrepriseAleatoire->setAdresse($faker->address);
+            $entrepriseAleatoire->setActivite("activite");
+            $entrepriseAleatoire->setUrlSite("https://siteEtreprise".$idEntreprise);
+
+            array_push($tabEntreprise,$entrepriseAleatoire);
+
+            $manager->persist($entrepriseAleatoire);
+
+            //creation de formation
+            
+            //creation de la relation entre stage et formation
+            
+            //creation de la relation entre stage et entreprise
+        }
+        //creation des Formations
+        $formationDUTInfo = new Formation();
+        $formationDUTInfo->setNomLong("DUT informatique");
+        $formationDUTInfo->setNomCourt("DUT info");
+
+        $formationLPNum = new Formation();
+        $formationLPNum->setNomLong("Licence Professionnelle Métiers du Numérique");
+        $formationLPNum->setNomCourt("LP Num");
+        
+
+        $formationLPProg = new Formation();
+        $formationLPProg->setNomLong("Licence Professsionnelle Programmation avancée");
+        $formationLPProg->setNomCourt("LP Prog");
+
+        $tabFormations = array($formationDUTInfo,$formationLPNum,$formationLPProg);
+        //creation des données de Stage
+        for($i = 0; $i < 20; $i++){
+
+            $nbAleatoireEntreprise = $faker->numberBetween($min = 0, $max = 9);
+            $nbAleatoireFormation = $faker->numberBetween($min = 0, $max = 2);
+
+            //creation d'un stage a partir de valeurs aléatoires
+            $stageAleatoire = new Stage();
+
+            $stageAleatoire->setTitre($faker->realText($maxNbChars = 30, $indexSize = 2));
+            $stageAleatoire->setDescriptionMission($faker->realText($maxNbChars = 250, $indexSize = 2));
+            $stageAleatoire->setEmailContact($faker->safeEmail);
+
+            //lier le stage à une entrepries aléatoire
+            $stageAleatoire->setEntreprise($tabEntreprise[$nbAleatoireEntreprise]);
+            //lier le stage à une formation aléatoire
+            $stageAleatoire->addFormation($tabFormations[$nbAleatoireFormation]);
+            
+
+            $manager->persist($stageAleatoire);
+        }
+
+        $manager->persist($formationDUTInfo);
+        $manager->persist($formationLPNum);
+        $manager->persist($formationLPProg);
         $manager->flush();
     }
 }
